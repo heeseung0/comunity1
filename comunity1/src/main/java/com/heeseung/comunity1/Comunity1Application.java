2c518c3eca9a880dfcc7f313f246a5067e113fd6
heeseung <ys2645@naver.com>
2022-11-15 19:38:55 +0900

    프로젝트 생성

---
+++ comunity1/src/main/java/com/heeseung/comunity1/Comunity1Application.java
@@ -0,0 +1,13 @@
+package com.heeseung.comunity1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Comunity1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Comunity1Application.class, args);
+	}
+
+}
