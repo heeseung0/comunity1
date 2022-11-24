71f84f66d5c99564ca5762096dff917c119919d9
heeseung <ys2645@naver.com>
2022-11-24 17:39:55 +0900

    게시글 내용물 도안 완성

--- comunity1/src/main/java/com/heeseung/community1/controller/BoardController.java
+++ comunity1/src/main/java/com/heeseung/community1/controller/BoardController.java
@@ -2,6 +2,7 @@ package com.heeseung.community1.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @Controller
 public class BoardController {
@@ -9,5 +10,10 @@ public class BoardController {
     @GetMapping("/Notice")
     public String notice(){
         return "board/notice";
+    }
+
+    @GetMapping("/Notice/{postNum}")
+    public String noticePost(@PathVariable String postNum){
+        return "board/noticePost";
     }
 }
